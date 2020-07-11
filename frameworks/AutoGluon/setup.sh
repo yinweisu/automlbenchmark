cd778829cac99dedec4800fbd1273600d7a49272
Nick Erickson <neerick@amazon.com>
2020-07-11 14:12:33 -0700

    Test further

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -11,7 +11,8 @@ fi
 
 cat $HERE/requirements.txt | sed '/^$/d' | while read -r i; do PIP install "$i"; done
 
-git clone https://github.com/awslabs/autogluon.git
+# git clone https://github.com/awslabs/autogluon.git
+git clone -b tabular_custom_model_support --single-branch https://github.com/awslabs/autogluon.git
 
 PIP install -e autogluon
 
