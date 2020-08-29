9507f8a7edbfa40a02fd4d87529461905345f4e0
Nick Erickson <neerick@amazon.com>
2020-08-28 17:00:48 -0700

    Test further (0.0.14 we multi)

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -9,11 +9,12 @@ if [[ -x "$(command -v brew)" ]]; then
     brew install libomp
 fi
 
-# cat $HERE/requirements.txt | sed '/^$/d' | while read -r i; do PIP install "$i"; done
+cat $HERE/requirements.txt | sed '/^$/d' | while read -r i; do PIP install "$i"; done
 PIP install -U --pre "mxnet>=1.7.0b20200713, <2.0.0" -f https://sxjscience.github.io/KDD2020/
 
 # git clone https://github.com/awslabs/autogluon.git
-git clone -b tabular_multi_layer_weighted_ensemble --single-branch https://github.com/awslabs/autogluon.git
+# git clone -b tabular_multi_layer_weighted_ensemble --single-branch https://github.com/awslabs/autogluon.git
+git clone -b nn_fastai_v2 --single-branch https://github.com/gradientsky/autogluon.git
 
 PIP install -e autogluon
 
