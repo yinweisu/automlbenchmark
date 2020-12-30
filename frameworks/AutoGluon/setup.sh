dbe6bbce6d9400ea6337e7eda609445abd710b29
Nick Erickson <neerick@amazon.com>
2020-12-30 14:38:17 -0800

    2020_12_30_fai

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -36,8 +36,8 @@ else
 #    PIP install --no-cache-dir -e git+${REPO}@${VERSION}#egg={PKG}
 
     # FIXME: HACK
-    VERSION="tabular_opt_gbm_learn_rate"
-    # REPO="https://github.com/gradientsky/autogluon.git"
+    VERSION="master"
+    REPO="https://github.com/gradientsky/autogluon.git"
 
     TARGET_DIR="${HERE}/lib/${PKG}"
     rm -Rf ${TARGET_DIR}
