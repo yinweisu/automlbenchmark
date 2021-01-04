d0b647a8896dd66c647f6bf894365da67942da65
Nick Erickson <neerick@amazon.com>
2021-01-04 13:52:16 -0800

    2021_01_04

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -9,7 +9,7 @@ if [[ "$VERSION" == "latest" ]]; then
 fi
 # FIXME: Hack
 if [[ "$VERSION" == "stable" ]]; then
-    VERSION="0.0.16b20201230"
+    VERSION="0.0.16b20210104"
 fi
 
 # creating local venv
