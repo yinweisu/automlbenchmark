af02840b9fe48c2ea1c6faa2e0f3177ca941eb6d
Nick Erickson <neerick@amazon.com>
2021-04-14 17:54:18 -0700

    2021_04_14_upgrade_cat

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -28,7 +28,7 @@ else
 #    PIP install --no-cache-dir -e git+${REPO}@${VERSION}#egg={PKG}
 
     # FIXME: HACK
-    VERSION="tabular_reorder_fastai"
+    VERSION="tabular_upgrade_cat"
     # REPO="https://github.com/gradientsky/autogluon.git"
 
     TARGET_DIR="${HERE}/lib/${PKG}"
