73ab6d624db9fcd7a0db5bd6c2e3ed6aeef67fd3
Nick Erickson <neerick@amazon.com>
2021-04-22 22:49:02 -0700

    2021_04_22_oob

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -28,7 +28,7 @@ else
 #    PIP install --no-cache-dir -e git+${REPO}@${VERSION}#egg={PKG}
 
     # FIXME: HACK
-    VERSION="tabular_repeat_5"
+    VERSION="tabular_efficient_oob"
     # REPO="https://github.com/gradientsky/autogluon.git"
 
     TARGET_DIR="${HERE}/lib/${PKG}"
