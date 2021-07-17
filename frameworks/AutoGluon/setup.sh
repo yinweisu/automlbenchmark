4e337ec163eb468c6b9f341e99ab2dd1874fbbbc
Nick Erickson <neerick@amazon.com>
2021-07-17 14:49:26 -0700

    2021_07_17

--- frameworks/AutoGluon/setup.sh
+++ frameworks/AutoGluon/setup.sh
@@ -26,7 +26,7 @@ elif [[ "$VERSION" =~ ^[0-9] ]]; then
     PIP install --no-cache-dir -U ${PKG}==${VERSION}
 else
     # FIXME: HACK
-    VERSION="tabular_stack_holdout"
+    # VERSION="tabular_stack_holdout"
     # REPO="https://github.com/gradientsky/autogluon.git"
 
     TARGET_DIR="${HERE}/lib/${PKG}"
